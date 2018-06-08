896fe0bc4ca56ca7ed0976bd6a93c42f240f0023
MrModest <babaevkamil@hotmail.com>
2018-06-08 16:31:13 +0300

    Add method source

--- ShikiNet/Utils/Extensions/UnderscoreExtentions.cs
+++ ShikiNet/Utils/Extensions/UnderscoreExtentions.cs
@@ -4,6 +4,7 @@ namespace ShikiNet.Utils.Extensions
 {
     public static class UnderscoreExtentions
     {
+        //https://gist.github.com/vkobel/d7302c0076c64c95ef4b
         public static string ToUnderscoreCase(this string str) {
             return string.Concat(str.Select((x, i) => (i > 0 && char.IsUpper(x)) ? "_" + x.ToString() : x.ToString())).ToLower();
         }
