7b99e4a64a3eadd14097a2ebcf41b384826aec20
MrModest <babaevkamil@hotmail.com>
2018-06-08 19:54:22 +0300

    prepare for refactoring enums to PascalCase

--- ShikiNet/Filter/FilterDictionary.cs
+++ ShikiNet/Filter/FilterDictionary.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using ShikiNet.Utils.Extensions;
 
 namespace ShikiNet.Filter
 {
@@ -89,6 +90,11 @@ namespace ShikiNet.Filter
             foreach (var filter in filterDict)
             {
                 //ToDo: convert key from PascalCase to under_score only for enum
+                // * Before this uncomment change all enums to PascalCase
+                // var key = filter.Key.ToString().ToLower();
+                // if (typeof(T).IsEnum){
+                //     key = filter.Key.ToString().ToUnderscoreCase();
+                // }
                 query.Append(filter.Value ? "" : "!").Append(filter.Key.ToString().ToLower()).Append(",");
             }
             query.Remove(query.Length - 1, 1); //delete last comma
